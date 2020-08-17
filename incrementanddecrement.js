296f5f5afad76b41b0b2410d3a08a5ffa32a6611
saurabhchobey <52131942+saurabhchobey@users.noreply.github.com>
2020-08-17 23:20:39 +0530

    Add files via upload

---
+++ incrementanddecrement.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+
+
+
+
+const App=()=>{
+  const[num,setNum]=useState(0);
+
+  const incNum=()=>
+  {
+    setNum(num+1);
+  }
+  const decNum=()=>{
+    
+    if (num>0)
+    {setNum(num-1);}
+    else{
+      alert("limit cross")
+    }
+  }
+
+
+
+  return(
+    <>
+    <div>
+     <div>
+     {num}
+      </div>
+       <div>
+       <button onClick={incNum}>
+       
+
+        </button  ><div>
+         <button onClick={decNum}>
+          </button>
+        </div></div>
+         </div>
+
+    </>
+    )
+}
+
+export default App;
